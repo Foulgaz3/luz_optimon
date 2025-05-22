13ea5cd51cb3324eeaffefcf0843cfd8167b7e3d
Foulgaz3 <tkdayden@gmail.com>
2025-05-22 14:59:09 -0400

    determines most recent start point of periodic schedule successfully

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,8 @@ use std::fs;
 
 use chrono::{DateTime, TimeDelta, Utc};
 use schedules::PeriodicSchedule;
-use std::time;
 
 use lunaluz_deserialization::*;
-
-fn hours_to_td(hours: Numeric) -> TimeDelta {
-    let seconds = f64::from(hours) * 3.6e3;
-    let duration = time::Duration::try_from_secs_f64(seconds).unwrap();
-    TimeDelta::from_std(duration).unwrap()
-}
-
-fn convert_times(times: Vec<Numeric>) -> Vec<TimeDelta> {
-    times.into_iter().map(hours_to_td).collect()
-}
 
 fn fetch_index(times: &[TimeDelta], time: &TimeDelta) -> usize {
     match times.binary_search(time) {
@@ -53,41 +42,56 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let start_date: DateTime<Utc> = parsed.info.start_date.parse().unwrap();
     let start_offset: iso8601_duration::Duration = parsed.info.start_offset.parse().unwrap();
     let start_offset = TimeDelta::from_std(start_offset.to_std().unwrap()).unwrap();
-    // let period = TimeDelta::hours(schedule.period);
     dbg!(start_date);
     dbg!(start_offset);
 
     // ! when converting times / schedule, need to assert they are in sorted order wrt time;
     // - Maybe just add this as a part of the specification and add debug check;
 
-    let period = hours_to_td(schedule.period.unwrap());
-    let times = schedule.times.map(convert_times).unwrap();
-    let values = schedule.values.unwrap();
-
     let default_val = parsed.variable_type_specs[&schedule.variable_type]
         .default
         .clone();
 
-    let periodic = PeriodicSchedule {
+    let periodic = PeriodicSchedule::new(
         start_date,
         start_offset,
-        period,
-        times,
-        values,
-        default_val
-    };
+        schedule.period.unwrap(),
+        schedule.times.unwrap(),
+        schedule.values.unwrap(),
+        default_val,
+    );
 
-    let ref_times = [0, 3, 6, 9, 12, 15, 18, 21, 24];
-    for time in ref_times {
-        let time = TimeDelta::hours(time);
-        let idx = fetch_index(&periodic.times, &time);
-        let value = if idx == 0 && time < periodic.times[0] {
-            periodic.default_val.clone()
-        } else {
-            periodic.values[idx].clone()
-        };
-        println!("Time: {time}, Value: {}", value);
-    }
+    let start_offset = periodic.start_offset.clone();
+    let start_point = periodic.start_date.clone() + start_offset;
+    let theoretic_day: DateTime<Utc> = "2025-05-23T19:36:56+00:00".parse().unwrap();
+
+    let elapsed = theoretic_day - (start_point + start_offset);
+    let approx_n = elapsed.num_seconds() / periodic.period.num_seconds();
+    let most_recent_start = start_point + periodic.period * approx_n as i32;
+    // ! May need to add / subtract by a period until
+    //   most_recent_start is the maximum solution to S = start + k*period
+    //   where S still comes before theoretic datetime
+    debug_assert!(most_recent_start < theoretic_day);
+    let schedule_time = theoretic_day - most_recent_start;
+    debug_assert!(schedule_time < periodic.period);
+
+    println!("Start: {start_point}, Current: {theoretic_day}");
+    dbg!(elapsed.num_hours());
+    dbg!(approx_n);
+    dbg!(periodic.period.num_hours());
+    dbg!(most_recent_start);
+
+    // let ref_times = [0, 3, 6, 9, 12, 15, 18, 21, 24];
+    // for time in ref_times {
+    //     let time = TimeDelta::hours(time);
+    //     let idx = fetch_index(&periodic.times, &time);
+    //     let value = if idx == 0 && time < periodic.times[0] {
+    //         periodic.default_val.clone()
+    //     } else {
+    //         periodic.values[idx].clone()
+    //     };
+    //     println!("Time: {time}, Value: {}", value);
+    // }
 
     Ok(())
 }
--- src/schedules.rs
+++ src/schedules.rs
@@ -1,4 +1,21 @@
+use std::default;
+
 use chrono::{TimeDelta, DateTime, Utc};
+use serde_json::Value;
+
+use crate::lunaluz_deserialization::Numeric;
+
+pub fn hours_to_td(hours: Numeric) -> TimeDelta {
+    let seconds = f64::from(hours) * 3.6e3;
+    let duration = std::time::Duration::try_from_secs_f64(seconds).unwrap();
+    TimeDelta::from_std(duration).unwrap()
+}
+
+pub fn convert_times(times: Vec<Numeric>) -> Vec<TimeDelta> {
+    times.into_iter().map(hours_to_td).collect()
+}
+
+// ! Should be able to remove start_offset from here and turn it into a construction thing
 
 pub struct PeriodicSchedule<T> {
     pub start_date: DateTime<Utc>,
@@ -7,4 +24,19 @@ pub struct PeriodicSchedule<T> {
     pub times: Vec<TimeDelta>,
     pub values: Vec<T>,
     pub default_val: T,
+}
+
+impl<T> PeriodicSchedule<T> {
+    pub fn new(start_date: DateTime<Utc>, start_offset: TimeDelta, period: Numeric, times: Vec<Numeric>, values: Vec<T>, default_val: T) -> Self {
+        let period = hours_to_td(period);
+        let times = convert_times(times);
+        Self {
+            start_date,
+            start_offset,
+            period,
+            times,
+            values,
+            default_val
+        }
+    }
 }
