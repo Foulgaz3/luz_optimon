29f858df557c6450241127222d9c2d74bd6628f8
Foulgaz3 <tkdayden@gmail.com>
2025-05-27 19:19:37 -0400

    basic implementation of server

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,16 @@ use std::{
     sync::{Arc, OnceLock},
 };
 
-use chrono::{DateTime, TimeDelta, Utc};
+use axum::{
+    routing::{get, post},
+    Json, Router,
+};
+
+use chrono::{DateTime, Utc};
 
 use lunaluz_deserialization::*;
 use schedules::{parse_schedules, Schedule, VarSchedule};
+use serde::Serialize;
 use serde_json::Value;
 
 type ScheduleMap = Arc<HashMap<String, Schedule<Value>>>;
@@ -21,10 +27,35 @@ pub fn schedules() -> ScheduleMap {
     SCHEDULES.get().expect("SCHEDULES not initialized").clone()
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[derive(Serialize)]
+struct ScheduleResponse {
+    time: DateTime<Utc>,
+    values: HashMap<String, String>,
+}
+
+async fn fetch_variable() -> Json<ScheduleResponse> {
+    let time: DateTime<Utc> = Utc::now();
+    let binding = schedules();
+
+    let mut response = ScheduleResponse {
+        time,
+        values: HashMap::new(),
+    };
+
+    for var in binding.keys() {
+        response.values.insert(
+            var.to_string(),
+            binding[var].floor_search(&time).to_string(),
+        );
+    }
+    Json(response)
+}
+
+#[tokio::main]
+async fn main() {
     let json_path = "../example_schedules/example_1.json";
-    let json_data = fs::read_to_string(json_path)?;
-    let parsed: ScheduleFile = serde_json::from_str(&json_data)?;
+    let json_data = fs::read_to_string(json_path).unwrap();
+    let parsed: ScheduleFile = serde_json::from_str(&json_data).unwrap();
 
     println!("Experiment: {}", parsed.info.experiment_name);
     println!("Variables: {}", parsed.variable_type_specs.len());
@@ -33,35 +64,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let map: ScheduleMap = Arc::new(parse_schedules(parsed.clone()));
     SCHEDULES.set(map).unwrap();
 
-    for (name, sched) in parsed.variable_schedules.into_iter() {
-        println!(" - {}: {:?}", name, sched.schedule_type());
-    }
+    let app = Router::new().route("/", get(fetch_variable));
 
-    dbg!(&parsed.variable_type_specs);
-
-    dbg!(&parsed.info);
-
-    let binding = schedules();
-    let schedule = &binding.get("red_led.duty_cycle");
-    dbg!(schedule);
-
-    // ! when converting times / schedule, need to assert they are in sorted order wrt time;
-    // - Maybe just add this as a part of the specification and add debug check;
-
-    let ref_time: DateTime<Utc> = "2025-05-23T00:00:00+00:00".parse().unwrap();
-    let ref_times = [0, 3, 6, 9, 12, 15, 18, 21, 24];
-    let times: Vec<DateTime<Utc>> = ref_times
-        .into_iter()
-        .map(|v| ref_time + TimeDelta::hours(v))
-        .collect();
-
-    for var_name in ["red_led.duty_cycle", "green_led.duty_cycle"] {
-        println!("Variable: {var_name}");
-        let values = binding.get(var_name).unwrap().floor_multi_search(&times);
-        for (time, value) in times.iter().zip(values) {
-            println!("Time: {time}, Value: {}", value);
-        }
-    }
-
-    Ok(())
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
